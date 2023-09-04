eb714847d0085428d76977aa4aad2c937417f48f
parampreet singh <parampreetsingh1092@gmail.com>
2023-09-04 09:41:46 +0530

    usestate hook

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-const ArrowFunction = params =>{
-  return (
-    <div>
-      <h1>
-        i am arrow function
-      </h1>
-    </div>
-  )
-}
+import React ,{ useState}from "react";
+
+// const ArrowFunction = params =>{
+//   return (
+//     <div>
+//       <h1>
+//         i am arrow function
+//       </h1>
+//     </div>
+//   )
+// }
 
 function CompA(){
+  //[valuestate contains two elements 1 is the state value and 2nd element is the elements through which we can change the state]
   return(
     <>
-   <ArrowFunction/>
    <h1>CompA</h1> 
    <p>hello Comp A</p>
    </>
@@ -47,8 +48,18 @@ function CompA(){
 }
 
 function Home(){
+    //[valuestate contains array two elements 1 is the state value and 2nd element is the elements through which we can change the state,mutatestate]
+  //different methods of accesing the elements of the array
+    // const valueState = useState(10);
+  const [value,setValue] = useState(10);
+  // const value = valueState[0];
+  // const setValue= valueState[1]
+  // console.log(valueState)
+  // console.log(valueState[0])
+
   return(
     <>
+      valueState :{valueState[0] }
      <h1> hello world</h1>
      <CompA />
     </>
